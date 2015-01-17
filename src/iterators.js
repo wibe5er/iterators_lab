881255d7aca459ad2968c10cfefd1e0fb5730473
Tim Licata <tim@timothylicata.com>
2015-01-16 16:41:38 -0800

    add tripler function and test

--- src/iterators.js
+++ src/iterators.js
@@ -1,7 +1,10 @@
 var Iterators = {
-  // Require them to use .forEach()
-  useForEach: function () {
-    return false;
+  // Tripler should accept an array of numbers as a parameter and
+  // return an array of the triples of those numbers. For example,
+  // - tripler([1,2,3]) should return [3,6,9], and
+  // - tripler([7,50,4]) should return [21,150,12].
+  // Use `map` to accomplish this.
+  tripler: function (numbers) {
   }
 };
 
--- test/iterators_test.js
+++ test/iterators_test.js
@@ -2,9 +2,12 @@ var expect = require("chai").expect;
 var Iterators = require("../src/iterators.js");
 
 describe("Iterators", function () {
-  describe("#useForEach", function () {
-    it("should work", function () {
-      expect(Iterators.useForEach([])).to.be.ok();
+  describe("#tripler", function () {
+    it("should map over numbers and triple them", function () {
+      expect(Iterators.tripler([1,2,3])).to.deep.equal([3,6,9]);
+      expect(Iterators.tripler([0,0,0])).to.deep.equal([0,0,0]);
+      expect(Iterators.tripler([-1,-2,-3])).to.deep.equal([-3,-6,-9]);
+      expect(Iterators.tripler([100,99,98])).to.deep.equal([300,297,294]);
     });
   });
 });
